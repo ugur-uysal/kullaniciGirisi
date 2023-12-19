991f28352950b43b037f93677200e00ec9761e0f
ugur <uysallugur@outlook.com>
2023-12-19 19:19:09 +0300

    reformat code

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,18 @@ public class Main {
         System.out.print("Şifre : ");
         password = inp.nextLine();
 
-        if(userName.equals("Patika") && password.equals("Java123")){
+        if (userName.equals("Patika") && password.equals("Java123")) {
             System.out.println("Giriş Başarılı !");
-        } else{
+        } else {
             System.out.println("Yanlış Şifre Girdiniz !");
             System.out.print("Şifrenizi değiştirmek ister misiniz ? (y/n) : ");
             select = inp.nextLine();
 
-            if(select.equals("y")){
+            if (select.equals("y")) {
                 System.out.print("Yeni Şifre : ");
                 newPassword = inp.nextLine();
 
-                if(newPassword.equals(password) || newPassword.equals("Java123"))
+                if (newPassword.equals(password) || newPassword.equals("Java123"))
                     System.out.println("Şifre oluşturulamadı, lütfen başka şifre giriniz.");
                 else
                     System.out.println("Şifre oluşturuldu.");
